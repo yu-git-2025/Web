9d6b1b899d11874102a8b9606807495989dbe3c8
挽梦忆笙歌 <3386462048@qq.com>
2025-04-19 19:59:42 +0800

    网页初始代码

--- src/views/HomeView/index.vue
+++ src/views/HomeView/index.vue
@@ -3,8 +3,16 @@
 </script>
 
 <template>
-
-</template>
+    <div class="common-layout">
+      <el-container>
+        <el-header>Header</el-header>
+        <el-container>
+          <el-aside width="200px">Aside</el-aside>
+          <el-main>Main</el-main>
+        </el-container>
+      </el-container>
+    </div>
+  </template>
 
 <style scoped>
 
