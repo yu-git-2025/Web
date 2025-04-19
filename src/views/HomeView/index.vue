bb0528e28632907ea6cddedbc99d37095d9776de
挽梦忆笙歌 <3386462048@qq.com>
2025-04-19 20:35:24 +0800

    header-aside-main布局

--- src/views/HomeView/index.vue
+++ src/views/HomeView/index.vue
@@ -4,16 +4,39 @@
 
 <template>
     <div class="common-layout">
-      <el-container>
-        <el-header>Header</el-header>
-        <el-container>
-          <el-aside width="200px">Aside</el-aside>
-          <el-main>Main</el-main>
+      <el-container class="header-container-layout">
+        <el-header class="header-layout">Header</el-header>
+        <el-container class="main-container-layout">
+          <el-aside width="200px" class="aside-layout">Aside</el-aside>
+          <el-main class="main-layout">Main</el-main>
         </el-container>
       </el-container>
     </div>
   </template>
 
 <style scoped>
+    .common-layout {
+        height: 100vh;
+        box-sizing: border-box;
+    }
+    .header-container-layout {
+        height: 100%;
+    }
+    .main-container-layout {
+        height: calc(100% - 80px);
+        border: 2px solid #000000;
+    }
+    .header-layout {
+        height: 80px;
+        background-color: #d4dce3;
+    }
+    .aside-layout {
+        background-color: #2b8040;
+
+    }
+    .main-layout {
+        background-color: #409EFF;
+
+    }
 
 </style>
